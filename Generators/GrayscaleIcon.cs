86e3bf12762c087502def459f34e380b0ccad35e
Wil <wip-@users.noreply.github.com>
2014-03-06 13:11:59 +0900

    Add generators (gradation icons)

---
+++ Generators/GrayscaleIcon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace pngGenerator.Generators
+{
+    public class GrayscaleIcon : BaseGenerator
+    {
+        public override String Filename { get { return "grayscaleIcon"; } }
+        public override int BitmapWidth { get { return 255; } }
+        public override int BitmapHeight { get { return 9; } }
+
+        public override void FillPixelArray()
+        {
+            for (int x = 0; x < BitmapWidth; x++)
+            {
+                double val = (double)x/255;
+                NormalizedColor color = new NormalizedColor(1, val, val, val);
+                for (int y = 0; y < BitmapHeight; y++)
+                {
+                    SetPixelArgb(x, y, color);
+                }                
+            }
+        }
+    }
+}
---
+++ Generators/HueGradationIcon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace pngGenerator.Generators
+{
+    public class HueGradationIcon : BaseGenerator
+    {
+        public override String Filename { get { return "hueGradationIcon"; } }
+        public override int BitmapWidth { get { return 255; } }
+        public override int BitmapHeight { get { return 9; } }
+
+        public override void FillPixelArray()
+        {
+            for (int x = 0; x < BitmapWidth; x++)
+            {
+                NormalizedColor color;
+                if (x < BitmapWidth / 2)
+                    color = Helpers.HSL2RGB(0.5 + (double)x / BitmapWidth, 1.0, 0.5);
+                else
+                    color = Helpers.HSL2RGB(-0.5 + (double)x / BitmapWidth, 1.0, 0.5);
+                for (int y = 0; y < BitmapHeight; y++)
+                {
+                    SetPixelArgb(x, y, color);
+                }                
+            }
+        }
+    }
+}
